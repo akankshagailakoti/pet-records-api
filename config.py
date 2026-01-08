f89625ff97a3c7e1d8d44f0e02b157522929d82b
Akanksha Gailakoti <gailakotiakanksha123@gmail.com>
2026-01-08 12:49:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Stores connection details
+# Keeps credentials like host, user, and password safe and easy to change
+# include placeholder values, not the real password when pushing it to GitHub
+
+#Create your own config.py and fill these
+
+HOST = "type your host name"
+USER = "type your user"
+PASSWORD = "type your password"  
